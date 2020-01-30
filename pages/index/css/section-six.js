408270d43212b139c4b8ea61b6089d0767a831c4
Nahum Landau <nahuml@tikalk.com>
2020-01-30 11:33:46 +0200

    homa page - section six (wip)

---
+++ pages/index/css/section-six.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Styles = styled.div`
+  .section-six {
+    background: #ffffff;
+    padding-top: 50px;
+    padding-bottom: 60px;
+    text-align: center;
+  }
+  h2 {
+    font-size: 65px;
+    margin: 0;
+    margin-bottom: 50px;
+    text-transform: uppercase;
+  }
+`;
+
+export default Styles;
---
+++ pages/index/partials/sectionSix.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Styles from "../css/section-six";
+
+const SectionSix = () => {
+  return (
+    <Styles>
+      <div className="section-six">
+        <h2>STAY UPDATED</h2>
+      </div>
+    </Styles>
+  );
+};
+
+export default SectionSix;
